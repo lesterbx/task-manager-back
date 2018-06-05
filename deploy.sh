98c349e2467ee99103c3fe6c8b8ca1a3973f0092
Lester Perez <l.perez@kweekvijvernoord.nl>
2018-06-05 14:32:56 +0200

    Updated ports

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ sudo docker images purge
 sudo docker system prune -a -f
 sudo docker rmi task-manager
 sudo docker build -t task-manager .
-sudo docker run -p 80:80 --name task-manager -d task-manager 
+sudo docker run -p 3000:3000 --name task-manager -d task-manager 
